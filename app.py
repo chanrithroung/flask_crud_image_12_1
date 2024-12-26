962260b2487e20991ef0fa2e440aab815c3f6bb4
chanrithroung2@gmail.com <chanrithroung2@gmail.com>
2024-12-26 13:53:25 +0700

    update image

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import services
 
 app = Flask(__name__)
 
+# ---------------------------------------------
 @app.route("/")
 def index():
     return render_template("base.html")
 
+# ---------------------------------------------
 @app.route("/create-post", methods=["GET", "POST"])
 def create_post():
     if request.method == 'GET':
         return render_template('create.html')
-
     services.createPost(request.form, request.files)
     return "Post create success"
+
+
+# ---------------------------------------------
+@app.route('/list-product')
+def list_product():
+    products = services.getAllProducts()
+    return render_template('list_product.html', products=products)
+
+
+# -------------------------------------------------------
+@app.route("/edit/<id>", methods=["GET", "POST"])
+def edit(id):
+    if request.method == "GET":
+        return render_template("edit.html", product=services.get_one_product_by_id(id))
+    else:
+        services.update(request.form, request.files, id)
     
+
 if __name__ == '__main__':
     app.run(debug=True)
--- services.py
+++ services.py
@@ -17,11 +17,40 @@ def createPost(form, file) -> None:
     cursor.execute(query=query)
     con.commit()
 
+def getAllProducts() -> list:
+    con = dc()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM `products` ORDER BY `id` DESC")
+    products = cursor.fetchall()
+    return products
+
+
+def get_one_product_by_id(id):
+    con = dc()
+    cursor = con.cursor()
+    cursor.execute(f"SELECT * FROM `products` WHERE id = {id}")
+    product = cursor.fetchall()[0]
+    return product
 
     
 
 
+def update(form, file, id) -> None:
+    name = form['name']
 
+    sourcefile = file['thumbnail']
+    if sourcefile:
+        filename = upload_image(sourcefile=sourcefile)
+    else:
+        filename = form['old-thumbnail']
+        
+    des = form['description']
 
-
-
+    query = f"""
+        UPDATE `products` SET `name` = '{name}', `thumbnail` = '{filename}', `description` = '{des}') 
+        WHERE `id` = {id}
+    """
+    con = dc()
+    cursor = con.cursor()
+    cursor.execute(query=query)
+    con.commit()
